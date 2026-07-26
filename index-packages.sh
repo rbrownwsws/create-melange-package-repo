589957f140564d80521515e25133b294c237f004
Ross Brown <rbrownwsws@googlemail.com>
2026-07-26 19:51:33 +0100

    fix: don't mangle signing key name as verification relies on it

--- index-packages.sh
+++ index-packages.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [[ -n "${SIGNING_KEY:-}" && -z "${SIGNING_KEY_NAME:-}" ]]; then
+  echo "::error::You provided 'signing-key' but did not provide 'signing-key-name'" >&2
+  exit 1
+fi
+
+if [[ -n "${SIGNING_KEY_NAME:-}" && -z "${SIGNING_KEY:-}" ]]; then
+  echo "::error::You provided 'signing-key-name' but did not provide 'signing-key'" >&2
+  exit 1
+fi
+
 REPO_DIR=$(realpath "$REPO_DIR")
 
 echo "Creating indexes for repo at ${REPO_DIR}"
 
-# Get a temporary file to store the private signing key
-SIGNING_KEY_FILE=$(mktemp --tmpdir="${RUNNER_TEMP}" --suffix=.rsa)
+SIGNING_ARGS=()
+if [[ -n "${SIGNING_KEY:-}" ]]; then
+  # Get a temporary dir to store the private signing key
+  SIGNING_KEY_DIR=$(mktemp -d --tmpdir="${RUNNER_TEMP}")
+  SIGNING_KEY_FILE="${SIGNING_KEY_DIR}/${SIGNING_KEY_NAME}.rsa"
 
-# Make sure we delete the private signing key file at the end of this step
-trap 'rm -f "${SIGNING_KEY_FILE}"' EXIT
+  # Make sure we delete the private signing key file at the end of this step
+  trap 'rm -rf "${SIGNING_KEY_DIR}"' EXIT
 
-# Put the private signing key into a file
-chmod 600 "${SIGNING_KEY_FILE}"
-echo -n "${SIGNING_KEY}" > "${SIGNING_KEY_FILE}"
+  # Put the private signing key into a file
+  touch "${SIGNING_KEY_FILE}"
+  chmod 600 "${SIGNING_KEY_FILE}"
+  printf '%s' "${SIGNING_KEY}" > "${SIGNING_KEY_FILE}"
 
-# Store the public signing key in the repo root
-openssl rsa -in "${SIGNING_KEY_FILE}" -pubout -out "${REPO_DIR}/signing.rsa.pub"
+  # Store the public signing key in the repo root
+  openssl rsa -in "${SIGNING_KEY_FILE}" -pubout -out "${REPO_DIR}/${SIGNING_KEY_NAME}.rsa.pub"
+
+  SIGNING_ARGS=("--signing-key=${SIGNING_KEY_FILE}")
+fi
 
 for ARCH_DIR in "${REPO_DIR}"/*; do
   if [[ ! -d "${ARCH_DIR}" ]]; then
@@ -26,6 +43,13 @@ for ARCH_DIR in "${REPO_DIR}"/*; do
   ARCH=$(basename "${ARCH_DIR}")
 
   echo ""
-  echo "Indexing arch '${ARCH}'..."
-  melange index --signing-key="${SIGNING_KEY_FILE}" -o "${ARCH_DIR}/APKINDEX.tar.gz" "${ARCH_DIR}"/*.apk
+  echo "::group::Indexing arch '${ARCH}'"
+
+  melange \
+    index \
+    "${SIGNING_ARGS[@]}" \
+    -o "${ARCH_DIR}/APKINDEX.tar.gz" \
+    "${ARCH_DIR}"/*.apk
+
+  echo "::endgroup::"
 done
