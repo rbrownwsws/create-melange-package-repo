a168b22fdfafd1fd34bd037f670f03a6475d10e4
Ross Brown <rbrownwsws@googlemail.com>
2026-08-11 05:38:38 +0100

    feat: include signed attestation bundles in repo

--- collect-package-versions.sh
+++ collect-package-versions.sh
@@ -21,61 +21,93 @@ while true; do
   releases_json=$(curl -sSf "${AUTH_HEADER[@]}" \
     "${API_BASE}?per_page=${per_page}&page=${page}")
 
-  releases_count=$(jq 'length' <<<"${releases_json}")
-  if [[ "${releases_count}" -eq 0 ]]; then
+  mapfile -t releases < <(jq -c '.[]' <<<"${releases_json}")
+
+  if [[ "${#releases[@]}" -eq 0 ]]; then
     break
   fi
 
-  for release_idx in $(seq 0 $((releases_count - 1))); do
-    release_json=$(jq ".[${release_idx}]" <<<"${releases_json}")
-    tag=$(jq -r ".tag_name" <<<"${release_json}")
-    echo "Found release '${tag}'"
+  for release in "${releases[@]}"; do
+    tag=$(jq -r ".tag_name" <<<"${release}")
 
-    apk_assets_json=$(jq '[.assets[] | select(.name | endswith(".apk")) | {name: .name, size: .size, url: .browser_download_url}]' <<<"${release_json}")
+    echo "::group::Release '${tag}'"
 
-    packages_count=$(jq 'length' <<<"${apk_assets_json}")
+    assets_json=$(jq '[.assets[] | { name: .name, size: .size, url: .browser_download_url }]' <<<"${release}")
 
-    echo ""
-    echo "'${tag}' contains ${packages_count} package(s):"
-    jq -r '.[] | .name' <<<"${apk_assets_json}"
+    packages_json=$(jq '[.[] | select(.name | endswith(".apk"))]' <<<"${assets_json}")
+    attests_json=$(jq '[.[] | select(.name | endswith(".sigstore.json"))]' <<<"${assets_json}")
 
-    packages_total_size_bytes=$(jq "[.[].size] | add // 0" <<<"${apk_assets_json}")
+    packages_count=$(jq 'length' <<<"${packages_json}")
+    attests_count=$(jq 'length' <<<"${attests_json}")
 
-    potential_total_downloaded_bytes=$((total_downloaded_bytes + packages_total_size_bytes))
+    packages_size_bytes=$(jq '[.[] | .size] | add // 0' <<<"${packages_json}")
+    attests_size_bytes=$(jq '[.[] | .size] | add // 0' <<<"${attests_json}")
+    release_size_bytes=$((packages_size_bytes + attests_size_bytes))
 
-    if [[ "${potential_total_downloaded_bytes}" -gt "${LIMIT_BYTES}" ]]; then
+    echo "This release contains:"
+    echo "${packages_count} packages (${packages_size_bytes} bytes)"
+    echo "${attests_count} attests (${attests_size_bytes} bytes)"
+    echo "Total: ${release_size_bytes} bytes"
+
+    potential_downloaded_bytes=$((total_downloaded_bytes + release_size_bytes))
+
+    if [[ "${potential_downloaded_bytes}" -gt "${LIMIT_BYTES}" ]]; then
       echo ""
-      echo "Stopping: release '${tag}' would take us over the size limit"
+      echo "Stopping: release '${tag}' would take us $((potential_downloaded_bytes - LIMIT_BYTES)) bytes over the repo size limit"
       exit 0
     fi
 
-    for package_idx in $(seq 0 $((packages_count -1))); do
-      package_filename=$(jq -r ".[${package_idx}].name" <<<"${apk_assets_json}")
-      package_url=$(jq -r ".[${package_idx}].url" <<<"${apk_assets_json}")
+    mapfile -t packages < <(jq -c '.[]' <<<"${packages_json}")
 
-      if [[ "${package_filename}" =~ ^(.+)-([^-]+)-r([0-9]+)-([^-]+)-([^-]+)\.apk$ ]]; then
-          name="${BASH_REMATCH[1]}"
-          version="${BASH_REMATCH[2]}"
-          epoch="${BASH_REMATCH[3]}"
-          distro="${BASH_REMATCH[4]}"
-          arch="${BASH_REMATCH[5]}"
+    for package in "${packages[@]}"; do
+      package_filename=$(jq -r '.name' <<<"${package}")
+      package_url=$(jq -r '.url' <<<"${package}")
+      package_prefix="${package_filename%.apk}"
 
-          # TODO: What do we do with distro? Error out if not what we expect?
-      else
-          echo "Failed to parse: ${package_filename}" >&2
+      echo ""
+      echo "${package_prefix}:"
+
+      if [[ ! "${package_prefix}" =~ ^(.+)-([^-]+)-r([0-9]+)-([^-]+)-([^-]+)$ ]]; then
+          echo "::error::Failed to parse package filename: ${package_prefix}"
           exit 1
       fi
 
-      echo "Downloading ${package_filename}"
+      package_name="${BASH_REMATCH[1]}"
+      package_version="${BASH_REMATCH[2]}"
+      package_epoch="${BASH_REMATCH[3]}"
+      package_distro="${BASH_REMATCH[4]}"
+      package_arch="${BASH_REMATCH[5]}"
 
-      target_dir="${REPO_DIR}/${arch}"
+      # TODO: What do we do with distro? Error out if not what we expect?
+
+      package_repo_prefix="${package_name}-${package_version}-r${package_epoch}"
+
+      target_dir="${REPO_DIR}/${package_arch}"
       mkdir -p "${target_dir}"
 
-      curl -sSfL "${AUTH_HEADER[@]}" -o "${target_dir}/${name}-${version}-r${epoch}.apk" "${package_url}"
+      echo "  - Downloading \"${package_filename}\" ..."
+      curl -sSfL "${AUTH_HEADER[@]}" -o "${target_dir}/${package_repo_prefix}.apk" "${package_url}"
 
+      mapfile -t attests < <(jq -c --arg package_prefix "${package_prefix}" '.[] | select(.name | startswith($package_prefix))' <<<"${attests_json}")
+      for attest in "${attests[@]}"; do
+        attest_filename=$(jq -r '.name' <<<"${attest}")
+        attest_url=$(jq -r '.url' <<<"${package}")
+
+        if [[ ! "${attest_filename}" =~ ^${package_prefix}(.*)\.sigstore.json$ ]]; then
+          echo "::error::Failed to parse attest filename: ${attest_filename}"
+          exit 1
+        fi
+
+        attest_qualifier="${BASH_REMATCH[1]}"
+
+        echo "  - Downloading \"${attest_filename}\" ..."
+        curl -sSfL "${AUTH_HEADER[@]}" -o "${target_dir}/${package_repo_prefix}${attest_qualifier}.sigstore.json" "${attest_url}"
+      done
     done
 
-    total_downloaded_bytes="${potential_total_downloaded_bytes}"
+    total_downloaded_bytes="${potential_downloaded_bytes}"
+
+    echo "::endgroup::"
   done
 
   page=$((page + 1))
